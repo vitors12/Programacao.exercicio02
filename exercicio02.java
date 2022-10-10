c29e44d30531e01f7e378bea0bd7b5ffe133f444
vitors12 <115507870+vitors12@users.noreply.github.com>
2022-10-10 19:26:03 -0300

    Add files via upload

---
+++ exercicio02.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class exercicio02 {
+    public static void main (String [] args) {
+        Scanner ler = new Scanner (System.in);
+            int numero;
+            System.out.print("Entre com um numero");
+            numero = ler.nextInt ();    
+            System.out.println("O numero informado foi " + numero);
+   }
+    
+}
